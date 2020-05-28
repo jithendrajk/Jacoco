b90658f808fe8f80c7974934b08e9205931f6084
jithendrajk <61737210+jithendrajk@users.noreply.github.com>
2020-05-28 09:32:30 +0530

    Update Comparestrings.java

--- src/main/java/compare/Comparestrings.java
+++ src/main/java/compare/Comparestrings.java
@@ -6,7 +6,7 @@ public class Comparestrings {
 	{
 		if(value.contains("Automation"))
 		{
-			System.out.println("we are software testersin JKT");
+			System.out.println("we are software testersin JKTECHNOSOFT");
 		}
 		else
 		{
